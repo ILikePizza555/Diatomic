4a1cb06090ca7b235f4d49b16a7f57c3f1f95410
bootstrap-prime <cyberman112sudo@gmail.com>
2021-02-15 21:03:40 -0500

    Added npm, vue-cli, auto install to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,10 +1,18 @@
 { pkgs ? import <nixpkgs> {} }:
 
+with pkgs;
+
 pkgs.mkShell {
   buildInputs = [
-    pkgs.nodejs-15_x
-
-    # keep this line if you use bash
-    pkgs.bashInteractive
+    nodejs-15_x
+    nodePackages.npm
+    nodePackages.vue-cli
   ];
+
+  shellHook = ''
+    if [ ! -d node_modules ]; then
+      npm install
+    fi
+    echo "run \"npm run serve\" to load local copy of service"
+  '';
 }
