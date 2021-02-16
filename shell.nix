e7248b5c1a85d59974dbb2bb9d985790b392dd37
bootstrap-prime <cyberman112sudo@gmail.com>
2021-02-15 21:41:58 -0500

    remove old packages, put bad solution for vue-cli because I really don't want to deal with node2nix

--- shell.nix
+++ shell.nix
@@ -5,13 +5,12 @@ with pkgs;
 pkgs.mkShell {
   buildInputs = [
     nodejs-15_x
-    nodePackages.npm
-    nodePackages.vue-cli
   ];
 
   shellHook = ''
     if [ ! -d node_modules ]; then
       npm install
+      npm i @vue/cli
     fi
     echo "run \"npm run serve\" to load local copy of service"
   '';
