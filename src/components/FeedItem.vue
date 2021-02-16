3518681434da49dd8284ea90228db86a25aee725
Izzy Lancaster <avrisaac555@gmail.com>
2021-02-15 23:34:11 -0800

    Added FeedItem.vue and deleted default component

---
+++ src/components/FeedItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <article>
+        <header>
+            <h2><a href="{{ url }}">{{ heading }}</a></h2>
+            <p v-if="subheading" class="subheading">{{ subheading }}</p>
+        </header>
+        <p v-if="summary">{{ summary }}</p>
+    </article>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+    name: "feed-item",
+    props: {
+        heading: String,
+        subheading: String,
+        url: String,
+        date: Date,
+        summary: String
+    }
+})
+</script>
