317276f8d88bcacc3103f7ef807c72aafb0561a2
Izzy <avrisaac555@gmail.com>
2021-03-28 21:50:56 -0700

    Added fastify-cookie as a plugin

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,13 @@
 import fastify from "fastify"
+import fastifyCookie from "fastify-cookie"
 import "make-promises-safe"
 
 const server = fastify({
     logger: true
 })
+
+// TODO: Properly set a secret here as an option once we get settings implemented.
+server.register(fastifyCookie)
 
 server.get("/", async (request, reply) => {
     return "Hello World!"
